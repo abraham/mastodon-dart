cb4135ad8766d297c838b3e9a46a0919baeeed33
Abraham Williams <4braham@gmail.com>
2025-10-30 22:03:14 -0500

    Generator 7.14

--- packages/mastodon_generator/lib/mastodon_generator.dart
+++ packages/mastodon_generator/lib/mastodon_generator.dart
@@ -1,4 +1,4 @@
-// Openapi Generator last run: : 2025-10-30T22:00:19.156821
+// Openapi Generator last run: : 2025-10-30T22:02:41.759374
 import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
 
 @Openapi(
